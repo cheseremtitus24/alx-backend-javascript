5411bba3c18ded6779ade1dc29a10a3737f5aba2
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-06-28 22:05:27 +0300

    Reformatting: Conform to eslint styling

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    let photo;
-    let user;
-    try {
-        user = await createUser();
-        photo = await uploadPhoto();
-    } catch (error) {
-        photo = null;
-        user = null;
-    }
-    return ({
-        photo,
-        user,
-    });
+  let photo;
+  let user;
+  try {
+    user = await createUser();
+    photo = await uploadPhoto();
+  } catch (error) {
+    photo = null;
+    user = null;
+  }
+  return ({
+    photo,
+    user,
+  });
 }
