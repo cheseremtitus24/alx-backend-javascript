4147f83296c3de23ee6aadbfd203339a88664c3d
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-06-28 14:34:11 +0300

    modified:   7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,17 @@
+/**
+ * 
+ * @param {*} chinaDownload 
+ * @param {*} USDownload 
+ * @returns 
+ −−−−−−−−−−−−−−−−−−−−+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−+−−−−−−−−−−−−−−−−−+
+| name               | description                                     |                 |
++−−−−−−−−−−−−−−−−−−−−+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−+−−−−−−−−−−−−−−−−−+
+| Promise.allSettled | does not short-circuit                          | Added in ES2020 |
+| Promise.all        | short-circuits when an input value is rejected  | Added in ES2015 |
+| Promise.race       | short-circuits when an input value is settled   | Added in ES2015 |
+| Promise.any        | short-circuits when an input value is fulfilled | this proposal   |
++−−−−−−−−−−−−−−−−−−−−+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−+−−−−−−−−−−−−−−−−−+
+ */
 function loadBalancer(chinaDownload, USDownload) {
   return Promise.race([chinaDownload, USDownload]);
 }
