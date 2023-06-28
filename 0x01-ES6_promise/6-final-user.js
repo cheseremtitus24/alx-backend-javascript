643d757b846f4d0fa0a240e7f86570e4bcdb2eb3
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-06-28 23:19:21 +0300

    modified:   6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,16 +4,9 @@ import uploadPhoto from './5-photo-reject';
 function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
-  const resultArray = new Array();
   const promises = [promise1, promise2];
 
   return Promise.allSettled(promises)
-    .then((results) => results.forEach((result) => {
-      if (result.status === 'rejected') {
-        result['value'] = result.reason;
-        delete result.reason;
-      }
-      resultArray.push(result);
-    })).then(() => resultArray);
+    .then((results) => results);
 }
 export default handleProfileSignup;
