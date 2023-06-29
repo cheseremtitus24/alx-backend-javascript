0db97690e9dda11c6a83820720cdcabdd91f576e
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-06-29 23:01:07 +0300

    modified:   8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -7,6 +7,7 @@ export default class HolbertonClass {
   // Setters
   set location(location) {
     if (typeof location === 'string') {
+      // eslint-disable-next-line no-underscore-dangle
       this._location = location;
     } else {
       throw new TypeError('location must be a string');
@@ -14,19 +15,42 @@ export default class HolbertonClass {
   }
 
   set size(size) {
-    if (isNaN(size)) {
+    if (Number.isNaN(size)) {
       throw new TypeError('size must be a number');
     } else {
+      // eslint-disable-next-line no-underscore-dangle
       this._size = size;
     }
   }
 
   // Getters
   get location() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._location;
   }
 
   get size() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._size;
+  }
+
+  // Primitive Method
+  /**
+   * +object JS calls object[Symbol.toPrimitive]('number');
+  ''+'object'  JS calls object[Symbol.toPrimitive]('string')
+  if you do
+  if(object) then it will call object[Symbol.toPrimitive]('default')
+   * @param {*} hint
+   * @returns
+   */
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this.location;
+    } if (hint === 'number') {
+      return this.size;
+    }
+    // when pushed, most classes (except Date)
+    // default to returning a number primitive
+    return this.size;
   }
 }
