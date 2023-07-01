af3d1308ebeaeecb8ee625132a8bb6bb8812a6cb
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-07-01 13:29:51 +0300

    modified:   3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-    return initialNumber + expansion1989 + expansion2019;
-}
+// eslint-disable-next-line max-len
+const getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
+export default getSumOfHoods;
