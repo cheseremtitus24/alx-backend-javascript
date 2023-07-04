8f1c8721e8bca683f1cb28ce0c0dc2e85001481c
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-07-04 15:05:37 +0300

    Refactoring: Simplified Function to make use of map function

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,14 +1,9 @@
 export default function getListStudentIds(myArray) {
-  const myMap = new Map();
   if (Array.isArray(myArray)) {
-    // iterate over the list of objects and set the id as
-    // key for the map
-    for (const obj of myArray) {
-      myMap.set(obj.id, obj);
-    }
-    // use of the splice operator
-    return myMap.keys();
-    // return Array.from(myMap.keys())
+    const result = myArray.map(
+      (obj) => obj.id,
+    );
+    return result;
   }
   return [];
 }
